399b4c5c601728f0c186ec74612af9c3ec54d479
satyam294 <satyamchand294@gmail.com>
2026-06-03 15:45:39 +0530

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,16 @@
-export let cart = [
-    {
-        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2
-    },
-    {
-        id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 1
-    }
-]
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
+if(!cart){
+    cart = [{
+                id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                quantity: 2
+            },
+            {
+                id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                quantity: 1
+            }
+        ]
+}
 // add products to cart
 export function addToCart(pressedItemId){
     let matchedInCart = '';
@@ -27,8 +29,7 @@ export function addToCart(pressedItemId){
             quantity: 1
         });
     }
-
-    console.log(cart);
+    saveCart();
 }
 
 // remove products from cart
@@ -42,4 +43,10 @@ export function removeFromCart(productId){
     })
 
     cart = newCart;
+    saveCart();
+}
+
+//save the cart to local storage
+function saveCart(){
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
