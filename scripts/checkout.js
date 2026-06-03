791942c308fab906a79bd64fdbb00f5b74c857c6
satyam294 <satyamchand294@gmail.com>
2026-06-03 14:52:07 +0530

    delete products from DOM when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -19,7 +19,7 @@ cart.forEach((cartItem) => {
 
     //generate the html for each cart item using fetched product details
     cartSummaryHTML += 
-    `<div class="cart-item-container">
+    `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
           Delivery date: Tuesday, June 21
         </div>
@@ -104,6 +104,8 @@ document.querySelectorAll('.js-delete-link')
     link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
+        //fetch container and remove it from the dom
+        document.querySelector(`.js-cart-item-container-${productId}`).remove();
         console.log(cart);
     })
 })
