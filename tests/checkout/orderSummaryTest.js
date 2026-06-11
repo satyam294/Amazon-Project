6baacd961dde40722534f2538d4c61f9b19e863c
satyam294 <satyamchand294@gmail.com>
2026-06-11 13:27:22 +0530

    load products in beforeAll hook for testing

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -5,12 +5,17 @@
 
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { cart, loadFromStorage } from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 
 const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
 const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
 
 describe('test suite: renderOrderSummary', () => {
 
+    beforeAll((done) => {
+        loadProducts(done);
+    })
+    
     // HOOK : the common code that runs before each test case [it()] bundled together
     beforeEach(() => {
         document.querySelector('.js-test-container').innerHTML = `
