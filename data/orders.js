b5354088a615d45da1837c77295a32420319748d
satyam294 <satyamchand294@gmail.com>
2026-06-15 11:43:27 +0530

    add getOrder() and getProductFromOrder() functions

--- data/orders.js
+++ data/orders.js
@@ -5,6 +5,28 @@ export function addOrder(order){
     saveOrders();
 }
 
+export function getOrder(orderId) {
+    let matchingOrder;
+    orders.forEach((order) => {
+        if(order.id === orderId){
+            matchingOrder = order;
+        }
+    });
+    return matchingOrder;
+}
+
+export function getProductFromOrder(productId, orderId) {
+    const order = getOrder(orderId);
+    let matchingProduct;
+
+    order.products.forEach((product) => {
+        if(product.productId === productId){
+            matchingProduct = product;
+        }
+    });
+    return matchingProduct;
+}
+
 function saveOrders(){
     localStorage.setItem('orders', JSON.stringify(orders));
 }
